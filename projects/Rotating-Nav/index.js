7fe4b5aee551597dd12bbbfc2dc5dad91fee0bc6
Rajshekarreddy78 <rajshekarreddyr07@gmail.com>
2023-04-08 18:47:40 +0530

    3 Rotating Navigation

--- projects/Rotating-Nav/index.js
+++ projects/Rotating-Nav/index.js
@@ -6,4 +6,7 @@ open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 
-// dsgg
+// // dsgg
+
+
+
